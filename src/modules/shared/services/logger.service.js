2452362e3580487bff0c3b271baa4a82e2747298
AeroreHighwind <gaston52343@gmail.com>
2024-03-28 16:59:44 -0300

    modify logger

--- src/modules/shared/services/logger.service.js
+++ src/modules/shared/services/logger.service.js
@@ -1,22 +1,10 @@
 export class ExpressLogger {
   static log = {
-    red: (text) => console.log("\x1b[31m" + getPrefix() + text + reset()),
-    green: (text) =>
-      console.log(
-        "\x1b[32m" + getPrefix() + reset() + getDate() + "\x1b[32m" + text
-      ),
-    yellow: (text) =>
-      console.log(
-        "\x1b[33m" + getPrefix() + reset() + getDate() + "\x1b[33m" + text
-      ),
-    blue: (text) =>
-      console.log(
-        "\x1b[34m" + getPrefix() + reset() + getDate() + "\x1b[34m" + text
-      ),
-    cyan: (text) =>
-      console.log(
-        "\x1b[36m" + getPrefix() + reset() + getDate() + "\x1b[36m" + text
-      ),
+    red: (text) => console.log("\x1b[31m" + getConcat() + "\x1b[31m" + text),
+    green: (text) => console.log("\x1b[32m" + getConcat() + "\x1b[32m" + text),
+    yellow: (text) => console.log("\x1b[33m" + getConcat() + "\x1b[33m" + text),
+    blue: (text) => console.log("\x1b[34m" + getConcat() + "\x1b[34m" + text),
+    cyan: (text) => console.log("\x1b[36m" + getConcat() + "\x1b[36m" + text),
   };
 
   static logRouterEndpoints(router) {
@@ -42,4 +30,8 @@ function getSufix() {
 }
 function getDate() {
   return `${new Date().toLocaleString("es-AR")} `;
+}
+
+function getConcat() {
+  return getPrefix() + reset() + getDate();
 }
